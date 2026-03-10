4ceb509f68f5bb56ddfbeef99f2dd7e2a4f0daff
HitomiTachi <lethanhnhanminer@gmail.com>
2026-03-10 22:18:41 +0700

    build: add project dependencies

---
+++ webdienthoai/webdienthoai/src/main/java/com/example/webdienthoai/WebdienthoaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdienthoai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdienthoaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdienthoaiApplication.class, args);
+	}
+
+}
---
+++ webdienthoai/webdienthoai/src/test/java/com/example/webdienthoai/WebdienthoaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdienthoai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdienthoaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
